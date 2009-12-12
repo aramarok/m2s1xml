f8f5e858ce5b7dbb972221fccbec6f61fc0cca2d
sergiunix <sergiunix@gmail.com>
2009-12-12 17:16:15 +0000

    added ws for search

--- src/p1/Cautare.java
+++ src/p1/Cautare.java
@@ -1,6 +1,6 @@
 package p1;
 
-import java.io.IOException;
+import java.util.HashMap;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -13,45 +13,103 @@ import javax.xml.xpath.XPathFactory;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
 public class Cautare {
 
-	@SuppressWarnings("unused")
-	private static String Q_AUTORI = "//bibtex:entry//*[bibtex:author[contains(upper-case(text()), upper-case('ca'))]]";
-	@SuppressWarnings("unused")
-	private static String Q_ANI = "//bibtex:entry//*[bibtex:year[text() = 2006]]";
-	@SuppressWarnings("unused")
-	private static String Q_CUVINTE_CHEIE = "//bibtex:entry//*[bibtex:keywords[contains(upper-case(text()), upper-case('mm'))]]";
-	@SuppressWarnings("unused")
-	private static String Q_REVISTA = "//bibtex:entry//*[bibtex:publisher[contains(upper-case(text()), upper-case('s'))]]";
-	@SuppressWarnings("unused")
-	private static String Q_EDITURA = "//bibtex:entry//*[bibtex:editor[contains(upper-case(text()), upper-case('be'))]]";
+	private static String Q_AUTORI = "//bibtex:entry//*[bibtex:author[contains(upper-case(text()), upper-case('QPARAM'))]]";
+	private static String Q_ANI = "//bibtex:entry//*[bibtex:year[text() = QPARAM]]";
+	private static String Q_CUVINTE_CHEIE = "//bibtex:entry//*[bibtex:keywords[contains(upper-case(text()), upper-case('QPARAM'))]]";
+	private static String Q_REVISTA = "//bibtex:entry//*[bibtex:publisher[contains(upper-case(text()), upper-case('QPARAM'))]]";
+	private static String Q_EDITURA = "//bibtex:entry//*[bibtex:editor[contains(upper-case(text()), upper-case('QPARAM'))]]";
 
-	public static void main(String[] args) throws ParserConfigurationException,
-			SAXException, IOException, XPathExpressionException {
+	public static void main(String[] args) {
 
-		DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory
-				.newInstance();
-		documentBuilderFactory.setNamespaceAware(true);
-
-		DocumentBuilder documentBuilder = documentBuilderFactory
-				.newDocumentBuilder();
-		Document document = documentBuilder.parse(Constants.XML_PATH);
-
-		XPathFactory xPathFactory = XPathFactory.newInstance();
-		XPath xPath = xPathFactory.newXPath();
-
-		xPath.setNamespaceContext(new BibTexPrefix());
-
-		XPathExpression xPathExpression = xPath.compile(Q_ANI);
-
-		Object object = xPathExpression.evaluate(document,
-				XPathConstants.NODESET);
-		NodeList listaNoduri = (NodeList) object;
-		for (int i = 0; i < listaNoduri.getLength(); i++) {
-			System.out.println(listaNoduri.item(i).getTextContent());
+		try {
+			search("ANI", "1974");
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 
+	}
+
+	public static HashMap<String, String> search(final String searchBy,
+			final String value) {
+
+		HashMap<String, String> outcome = new HashMap<String, String>();
+
+		String query = getQuery(searchBy, value);
+
+		if (query != null) {
+
+			DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory
+					.newInstance();
+			documentBuilderFactory.setNamespaceAware(true);
+
+			DocumentBuilder documentBuilder = null;
+			try {
+				documentBuilder = documentBuilderFactory
+						.newDocumentBuilder();
+			} catch (ParserConfigurationException e) {
+				e.printStackTrace();
+			}
+			
+			Document document = null;
+			try {
+				document = documentBuilder.parse(Constants.XML_PATH);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			XPathFactory xPathFactory = XPathFactory.newInstance();
+			XPath xPath = xPathFactory.newXPath();
+
+			xPath.setNamespaceContext(new BibTexPrefix());
+
+			XPathExpression xPathExpression = null;
+			try {
+				xPathExpression = xPath.compile(query);
+			} catch (XPathExpressionException e) {
+				e.printStackTrace();
+			}
+
+			Object object = null;
+			try {
+				object = xPathExpression.evaluate(document,
+						XPathConstants.NODESET);
+			} catch (XPathExpressionException e) {
+				e.printStackTrace();
+			}
+			NodeList listaNoduri = (NodeList) object;
+			for (int i = 0; i < listaNoduri.getLength(); i++) {
+				System.out.println(listaNoduri.item(i).getTextContent());
+				outcome.put(String.valueOf(i), listaNoduri.item(i).getTextContent());
+			}
+
+		}
+
+		return outcome;
+	}
+
+	private static String getQuery(String tip, String value) {
+
+		String query = null;
+
+		if ("AUTORI".equals(tip)) {
+			query = Q_AUTORI;
+		} else if ("ANI".equals(tip)) {
+			query = Q_ANI;
+		} else if ("CUVINTE_CHEIE".equals(tip)) {
+			query = Q_CUVINTE_CHEIE;
+		} else if ("REVISTA".equals(tip)) {
+			query = Q_REVISTA;
+		} else if ("EDITURA".equals(tip)) {
+			query = Q_EDITURA;
+		}
+
+		if (query != null) {
+			query = query.replace("QPARAM", value);
+		}
+
+		return query;
 	}
 }
