c86d0cb47f35b4aa1727defac73d86dbe2849824
dreytc <dreytc@c2a6bf3a-e199-11de-9e61-43d41527e390>
2010-01-08 09:46:03 +0000

    paths

--- trunk/src/oldP1/OldConstants.java
+++ trunk/src/oldP1/OldConstants.java
@@ -5,21 +5,21 @@ public interface OldConstants {
 	String ASSETS_PATH2 = "../webapps/m2s1xml/files/";
 
 	// bibtex
-	String BIBTEX_XML_DB = ASSETS_PATH + "bibtex.xml";
-	String BIBTEX_XSD = ASSETS_PATH + "bibtex.xsd";
+	String BIBTEX_XML_DB = ASSETS_PATH + "bibtex/bibtex.xml";
+	String BIBTEX_XSD = ASSETS_PATH + "bibtex/bibtex.xsd";
 
-	String BIBTEX_FOP_XSL = ASSETS_PATH + "bibtex.xsl";
-	String BIBTEX_FOP_OUT = ASSETS_PATH + "bibtex.pdf";
+	String BIBTEX_FOP_XSL = ASSETS_PATH + "bibtex/bibtex.xsl";
+	String BIBTEX_FOP_OUT = ASSETS_PATH + "bibtex/bibtex.pdf";
 
-	String BIBTEX_XSL_ADD = ASSETS_PATH + "bibtexAdd.xsl";
-	String BIBTEX_XSL_REMOVE = ASSETS_PATH + "bibtexRemove.xsl";
-	String BIBTEX_XSL_SEARCH = ASSETS_PATH + "bibtexSearch.xsl";
+	String BIBTEX_XSL_ADD = ASSETS_PATH + "bibtex/bibtexAdd.xsl";
+	String BIBTEX_XSL_REMOVE = ASSETS_PATH + "bibtex/bibtexRemove.xsl";
+	String BIBTEX_XSL_SEARCH = ASSETS_PATH + "bibtex/bibtexSearch.xsl";
 	
 	// myagenda
-	String XML_DB = ASSETS_PATH + "myagenda.xml";
-	String FOP_XSL = ASSETS_PATH + "xfo.xsl";
-	String FOP_OUT = ASSETS_PATH + "test.pdf";
+	String XML_DB = ASSETS_PATH + "test/myagenda.xml";
+	String FOP_XSL = ASSETS_PATH + "test/xfo.xsl";
+	String FOP_OUT = ASSETS_PATH + "test/test.pdf";
 
-	String XSL_ADD = ASSETS_PATH + "myagendaadd.xsl";
-	String XSL_RMV = ASSETS_PATH + "myagendarmv.xsl";
+	String XSL_ADD = ASSETS_PATH + "test/myagendaadd.xsl";
+	String XSL_RMV = ASSETS_PATH + "test/myagendarmv.xsl";
 }
