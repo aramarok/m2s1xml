1e2daa51ea8b6bc99b6334adb5e1d3c45437290d
dreytc <dreytc@c2a6bf3a-e199-11de-9e61-43d41527e390>
2010-01-08 11:18:00 +0000

    xslt remove bibtex entry added

--- src/m2s1xml/Constants.java
+++ src/m2s1xml/Constants.java
@@ -2,8 +2,8 @@ package m2s1xml;
 
 public interface Constants {
 
-	String ASSETS_PATH = "../m2s1xml/WebContent/files/bibtex";
-	String ASSETS_PATH2 = "../webapps/m2s1xml/files/bibtex";
+	String ASSETS_PATH = "../m2s1xml/WebContent/files/bibtex/";
+	//String ASSETS_PATH = "../webapps/m2s1xml/files/bibtex/";
 
 	String BIBTEX_XML_DB = ASSETS_PATH + "bibtex.xml";
 	String BIBTEX_XSD = ASSETS_PATH + "bibtex.xsd";
@@ -11,8 +11,8 @@ public interface Constants {
 	String BIBTEX_PDF_FOP_XSL = ASSETS_PATH + "bibtex.xsl";
 	String BIBTEX_PDF_FOP_OUT = ASSETS_PATH + "bibtex.pdf";
 
-	String BIBTEX_XSL_ADD = ASSETS_PATH + "bibtexAdd.xsl";
-	String BIBTEX_XSL_REMOVE = ASSETS_PATH + "bibtexRemove.xsl";
-
 	String BIBTEX_XSL_SEARCH = ASSETS_PATH + "bibtexSearch.xsl";
+	String BIBTEX_XSL_ADD = ASSETS_PATH + "bibtexAdd.xsl";
+	String BIBTEX_XSL_REMOVE = ASSETS_PATH + "bibtexRemoveV2.xsl";
+	
 }
