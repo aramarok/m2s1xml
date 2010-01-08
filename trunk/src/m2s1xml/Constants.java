18e43d9fb31d4e891d62596cc7914394e36b10eb
dreytc <dreytc@c2a6bf3a-e199-11de-9e61-43d41527e390>
2010-01-08 09:11:48 +0000

    new XmlBibtex

---
+++ trunk/src/m2s1xml/Constants.java
@@ -0,0 +1,17 @@
+package m2s1xml;
+
+public interface Constants {
+
+	String ASSETS_PATH = "../m2s1xml/WebContent/files/";
+	String ASSETS_PATH2 = "../webapps/m2s1xml/files/";
+
+	String BIBTEX_XML_DB = ASSETS_PATH + "bibtex.xml";
+	String BIBTEX_XSD = ASSETS_PATH + "bibtex.xsd";
+
+	String BIBTEX_PDF_FOP_XSL = ASSETS_PATH + "bibtex.xsl";
+	String BIBTEX_PDF_FOP_OUT = ASSETS_PATH + "bibtex.pdf";
+
+	String BIBTEX_XSL_ADD = ASSETS_PATH + "bibtexAdd.xsl";
+	String BIBTEX_XSL_REMOVE = ASSETS_PATH + "bibtexRemove.xsl";
+
+}
