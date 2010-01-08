2785df4d6e8d26ee1732f0b39d05d8451dc11196
dreytc <dreytc@c2a6bf3a-e199-11de-9e61-43d41527e390>
2010-01-08 09:17:33 +0000

    old p1

---
+++ trunk/src/oldP1/OldConstants.java
@@ -0,0 +1,24 @@
+package oldP1;
+
+public interface OldConstants {
+	String ASSETS_PATH = "../m2s1xml/WebContent/files/";
+	String ASSETS_PATH2 = "../webapps/m2s1xml/files/";
+
+	// bibtex
+	String BIBTEX_XML_DB = ASSETS_PATH + "bibtex.xml";
+	String BIBTEX_XSD = ASSETS_PATH + "bibtex.xsd";
+
+	String BIBTEX_FOP_XSL = ASSETS_PATH + "bibtex.xsl";
+	String BIBTEX_FOP_OUT = ASSETS_PATH + "bibtex.pdf";
+
+	String BIBTEX_XSL_ADD = ASSETS_PATH + "bibtexAdd.xsl";
+	String BIBTEX_XSL_REMOVE = ASSETS_PATH + "bibtexRemove.xsl";
+
+	// myagenda
+	String XML_DB = ASSETS_PATH + "myagenda.xml";
+	String FOP_XSL = ASSETS_PATH + "xfo.xsl";
+	String FOP_OUT = ASSETS_PATH + "test.pdf";
+
+	String XSL_ADD = ASSETS_PATH + "myagendaadd.xsl";
+	String XSL_RMV = ASSETS_PATH + "myagendarmv.xsl";
+}
